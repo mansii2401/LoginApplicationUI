86ff9a8485cc2701f9957198d43d523b8506542e
mansii2401 <141560164+mansii2401@users.noreply.github.com>
2023-09-08 09:26:35 +0000

    form alert

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,13 +1,40 @@
 <template>
-<div class="loginView">
-    <form >
-        <label for="username">Username</label>
-        <input type="text" name="username">
-        
-        <label for="password">Password</label>
-        <input type="text" name="password">
+    <div class="loginView">
+        <form @submit.prevent="onLoginFormSubmit">
+            <label for="username">Username</label>
+            <input type="text" name="username" v-model="username">
 
-        <input type="submit" value="Login">
-    </form>
-</div>
+            <label for="password">Password</label>
+            <input type="password" name="password" v-model="password">
+
+            <input type="submit" value="Login">
+        </form>
+
+        <div>
+            {{ username }}
+        </div>
+    </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const username = ref()
+const password = ref()
+
+
+function onLoginFormSubmit() {
+    fetch('https://verbose-carnival-jjqqgv49qj925pr4-5242.app.github.dev/api/Login', {
+        method: "POST",
+        body: JSON.stringify({
+            "userName": username.value,
+            "password": password.value
+        }),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+    }).then(x=>x.json()).then(x=>alert(x))
+}
+
+</script>
